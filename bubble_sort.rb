e2e3843177da5c6276f6eb08e55f81e9d6798779
Javi <jav.olivares3@gmail.com>
2022-02-05 11:50:16 -0600

    formating files

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+def bubble_sort(arr)
+  i = 0
+  sets = arr.length - 1
+  while sets > 0
+    arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+    i += 1
+    if i == sets
+      sets -= 1
+      i = 0
+    end
+  end
+  p arr
+end
+bubble_sort([4, 3, 78, 2, 0, 2])
--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,11 @@
-dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-def substrings (string, dictionary)
-    s_array = string.split.each {|a| a.downcase!}
-    subs = Hash.new(0)
-    s_array.each do |item| 
-        dictionary.each{|w| item.include?(w)? (subs[w] += 1) : nil}
-    end
-    puts subs
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+def substrings(string, dictionary)
+  s_array = string.split.each { |a| a.downcase! }
+  subs = Hash.new(0)
+  s_array.each do |item|
+    dictionary.each { |w| item.include?(w) ? (subs[w] += 1) : nil }
+  end
+  puts subs
 end
-substrings("below", dictionary)
+substrings('below', dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
