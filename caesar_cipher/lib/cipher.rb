c3b373d6aabaedc9b52235d1ce7f6a88b046368b
Javi <jav.olivares3@gmail.com>
2022-04-11 10:53:09 -0500

    modified formatting as per rubocop suggestions

--- caesar_cipher/lib/cipher.rb
+++ caesar_cipher/lib/cipher.rb
@@ -1,20 +1,14 @@
-require 'pry-byebug'
-
-#binding.pry
+# frozen_string_literal: true
 
 def caesar_cipher(string, shift)
   s_array = string.chars
   s_array.map! do |c|
     if c.ord.between?(65, 90) || c.ord.between?(97, 122)
       new_ord = c.ord + shift
-      c = if new_ord.between?(65, 90) || new_ord.between?(97, 122)
-            new_ord.chr
-          else
-            until new_ord.between?(65, 90) || new_ord.between?(97, 122)
-              new_ord = c.ord.between?(65, 90) ? 64 + (new_ord - 90) : 96 + (new_ord - 122)
-            end
-            new_ord.chr
-          end
+      until new_ord.between?(65, 90) || new_ord.between?(97, 122)
+        new_ord = c.ord.between?(65, 90) ? 64 + (new_ord - 90) : 96 + (new_ord - 122)
+      end
+      new_ord.chr
     else
       c
     end
--- caesar_cipher/spec/cipher_spec.rb
+++ caesar_cipher/spec/cipher_spec.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 require './lib/cipher'
 
-describe 'caesar_cipher' do 
-  it "returns a cipher of a short string with short shift" do
-    expect(caesar_cipher("hello", 4)).to eql("lipps")
+describe 'caesar_cipher' do
+  it 'returns a cipher of a short string with short shift' do
+    expect(caesar_cipher('hello', 4)).to eql('lipps')
   end
 
-  it "returns a cipher of long shift" do
-    expect(caesar_cipher("world", 50)).to eql("umpjb")
+  it 'returns a cipher of long shift' do
+    expect(caesar_cipher('world', 50)).to eql('umpjb')
   end
 
-  it "returns a cipher of string with capatalization" do
-    expect(caesar_cipher("Wonderful", 7)).to eql("Dvuklymbs")
+  it 'returns a cipher of string with capatalization' do
+    expect(caesar_cipher('Wonderful', 7)).to eql('Dvuklymbs')
   end
 
-  it "returns a cipher of multiple words" do
-    expect(caesar_cipher("Never going to give you up", 15)).to eql("Ctktg vdxcv id vxkt ndj je")
+  it 'returns a cipher of multiple words' do
+    expect(caesar_cipher('Never going to give you up', 15)).to eql('Ctktg vdxcv id vxkt ndj je')
   end
 
-  it "returns a cipher with capatalization, multiple words and punctuation intact" do
-    expect(caesar_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
+  it 'returns a cipher with capatalization, multiple words and punctuation intact' do
+    expect(caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
   end
-  
-  it "returns a cipher of large shift and string" do
-    expect(caesar_cipher("This is a secret, please do not show it to anyone!", 45)).to eql("gabl bl t lxvkxm, iextlx wh ghm lahp bm mh tgrhgx!")
+
+  it 'returns a cipher of large shift and string' do
+    expect(caesar_cipher('This is a secret, please do not show it to anyone!',
+                         45)).to eql('gabl bl t lxvkxm, iextlx wh ghm lahp bm mh tgrhgx!')
   end
 end
